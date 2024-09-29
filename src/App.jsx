e4db5dcfe64473a10b7cacd3b2d2cbeea33cf7a2
Maxim <besstrakhov.m@gmail.com>
2024-09-29 23:02:49 +0300

    change-0001

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
 import React, {useState} from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
     const [data, setData] = useState({});
 
     const givePrice = () => {
         if (data.price && data.discount) {
-            let result = data.price - (data.price * data.discount) / 100;
+            let result = `${data.price - (data.price * data.discount) / 100} nis`;
             return result;
         }
     };
     return (
         <div>
-            <h1>Lazy Ass </h1>
-            <div>
-                <label>
+            <h1 className=" text-center">Lazy Ass </h1>
+            <div className=" d-flex flex-column">
+                <label className="mx-auto">
                     price:
-                    <input type="number" onChange={(e) => setData({...data, price: e.currentTarget.value})} />
+                    <input type="number" className=" form-control" onChange={(e) => setData({...data, price: e.currentTarget.value})} />
                 </label>
-                <label>
+                <label className="mx-auto form-label">
                     discount(%):
-                    <input type="number" onChange={(e) => setData({...data, discount: e.currentTarget.value})} />
+                    <input type="number" className=" form-control" onChange={(e) => setData({...data, discount: e.currentTarget.value})} />
                 </label>
             </div>
             <div>
-                <h3>Total price: {givePrice()}</h3>
+                <h3 className="text-center">Total price: {givePrice()} </h3>
             </div>
         </div>
     );
